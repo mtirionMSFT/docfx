f18d8f362ff8040956e034fe878544b1d2dc6263
Yufei Huang <yufeih@live.com>
2023-03-28 09:29:14 +0800

    wip

--- src/Microsoft.DocAsCode.Dotnet/SymbolUrlResolver.cs
+++ src/Microsoft.DocAsCode.Dotnet/SymbolUrlResolver.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.CodeAnalysis;
+﻿using System.Text.RegularExpressions;
+using Microsoft.CodeAnalysis;
 
 #nullable enable
 
@@ -6,9 +7,31 @@ namespace Microsoft.DocAsCode.Dotnet;
 
 internal static partial class SymbolUrlResolver
 {
-    public static string? GetSymbolUrl(ISymbol symbol, Compilation compilation)
+    public static string? GetSymbolUrl(ISymbol symbol, Compilation compilation, HashSet<IAssemblySymbol> allAssemblies)
     {
-        return GetMicrosoftLearnUrl(symbol)
+        return GetDocfxUrl(symbol, allAssemblies)
+            ?? GetMicrosoftLearnUrl(symbol)
             ?? GetPdbSourceLinkUrl(compilation, symbol);
+    }
+
+    internal static string? GetDocfxUrl(ISymbol symbol, HashSet<IAssemblySymbol> allAssemblies)
+    {
+        if (symbol.ContainingAssembly is null || !allAssemblies.Contains(symbol.ContainingAssembly))
+            return null;
+
+        var commentId = symbol.GetDocumentationCommentId();
+        if (commentId is null)
+            return null;
+
+        var parts = commentId.Split(':');
+        var type = parts[0];
+        var uid = parts[1].ToLowerInvariant();
+
+        return type switch
+        {
+            "N" or "T" => $"{uid.Replace('`', '-')}.html",
+            "M" or "F" or "P" or "E" => $"{VisitorHelper.GetId(symbol.ContainingType).Replace('`', '-')}.html#{Regex.Replace(uid, @"/\W/", "_")}",
+            _ => throw new NotSupportedException($"Unknown comment ID format '{type}"),
+        };
     }
 }
